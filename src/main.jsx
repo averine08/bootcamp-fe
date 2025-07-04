97cecbd74632aa9d23756ec1a63d06dae03b00e7
averine08 <averinanurdin08@gmail.com>
2025-07-04 18:02:17 +0700

    finish update without library

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import AvePage from './views/2501981560/AvePage';
+
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}/>
+        <Route path='/' element={<AvePage/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
